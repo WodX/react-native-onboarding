9ef7b98e68801e7af7bfd8ed285bd85b6d534df3
Andre Nunes <a.sousa@runtime-revolution.com>
2021-05-25 18:38:13 +0100

    Add Profile Screen

--- src/slices/userSlice.js
+++ src/slices/userSlice.js
@@ -3,13 +3,18 @@ import {createSlice} from '@reduxjs/toolkit';
 export const userSlice = createSlice({
   name: 'user',
   initialState: {
-    name: '',
-    age: '',
-    email: '',
+    image: '',
+    name: 'André Nunes',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras malesuada ipsum ac nisi vestibulum mattis. Nullam vel ex est. Integer varius, mauris sed bibendum rutrum, justo odio tempor libero, eu aliquet lectus massa vel velit. Sed tortor metus, mollis ac porttitor a, efficitur eget elit. Phasellus suscipit fermentum nisl, efficitur pretium magna suscipit eu. Sed rhoncus urna neque, et consectetur sem congue ut.',
+    email: 'myemail@example.com',
   },
   reducers: {
     updateUser: (state, action) => {
-      state = action.payload;
+      state.image = action.payload.image || state.image;
+      state.name = action.payload.name || state.name;
+      state.description = action.payload.description || state.description;
+      state.email = action.payload.email || state.email;
     },
   },
 });
--- src/sreens/ProfileScreen.js
+++ src/sreens/ProfileScreen.js
@@ -1,14 +1,185 @@
-import * as React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Modal,
+  Pressable,
+  TextInput,
+} from 'react-native';
 import {useSelector, useDispatch} from 'react-redux';
-import {updateUser} from '../app/userSlice';
+import {updateUser} from '../slices/userSlice';
 
 function ProfileScreen({navigation}) {
+  const userData = useSelector(data => data.user);
+  const dispatch = useDispatch();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [user, setUser] = useState(userData);
+
+  const handleSave = () => {
+    setModalVisible(!modalVisible);
+    dispatch(updateUser(user));
+  };
+
   return (
-    <SafeAreaView>
-      <Text>ProfileScreen</Text>
+    <SafeAreaView style={styles.container}>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <View>
+              <TextInput
+                value={user.name}
+                style={styles.input}
+                onChangeText={text => setUser({...user, name: text})}
+              />
+              <TextInput
+                value={user.email}
+                style={styles.input}
+                onChangeText={text => setUser({...user, email: text})}
+              />
+              <TextInput
+                value={user.description}
+                style={styles.input}
+                onChangeText={text => setUser({...user, description: text})}
+                multiline
+              />
+            </View>
+            <Pressable
+              style={[styles.button, styles.buttonEdit]}
+              onPress={handleSave}>
+              <Text style={styles.textStyle}>Save</Text>
+            </Pressable>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
+      <View style={styles.header}>
+        <View style={styles.imageContainer}>
+          <Image source={require('../assets/user.png')} style={styles.image} />
+        </View>
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.name}>{user.name}</Text>
+        <Text style={styles.email}>{user.email}</Text>
+        <Text style={styles.description}>{user.description}</Text>
+        <Pressable
+          style={[styles.button, styles.buttonEdit]}
+          onPress={() => setModalVisible(!modalVisible)}>
+          <Text style={styles.textStyle}>Edit Profile</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#6d6875',
+    marginBottom: 50,
+  },
+  imageContainer: {
+    position: 'absolute',
+    bottom: -50,
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    backgroundColor: '#ced4da',
+    borderColor: 'white',
+    borderWidth: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 50,
+  },
+  content: {
+    flex: 2,
+    alignItems: 'center',
+    padding: 15,
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: '#343a40',
+  },
+  email: {
+    color: '#adb5bd',
+  },
+  description: {
+    marginVertical: 35,
+    color: '#343a40',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 10,
+    padding: 10,
+    elevation: 2,
+    minWidth: 100,
+    marginVertical: 5,
+  },
+  buttonClose: {
+    backgroundColor: '#f08080',
+  },
+  buttonEdit: {
+    backgroundColor: '#277da1',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  input: {
+    padding: 10,
+    backgroundColor: '#ced4da',
+    borderRadius: 10,
+    width: 300,
+    marginVertical: 10,
+  },
+});
 
 export default ProfileScreen;
